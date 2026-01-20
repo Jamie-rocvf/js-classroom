75bbc176a4222c162a5e62339c629001852c314b
Jamie-rocvf <2227916@talnet.nl>
2026-01-20 13:20:15 +0100

    Update app.js

--- FindTheBugJavaScript/Form-Validation/app.js
+++ FindTheBugJavaScript/Form-Validation/app.js
@@ -44,8 +44,8 @@ function verifyInputs() {
 
   if (confirmPassword === "") {
     dealErrorFor(confirmPasswordElem, "Confirm Password is required");
-  } else if (password !== confirmPassword) {
-    dealErrorFor(confirmPassword, `Password didn't match`);
+  } else if (password != confirmPassword) {
+    dealErrorFor(confirmPasswordElem, "Password didn't match");
   } else {
     dealSuccessFor(confirmPasswordElem);
   }
